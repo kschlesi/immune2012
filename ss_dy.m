48b2456239a22f3729d699d2f3dc96d2e9d10955
Kimberly <kschlesi42@gmail.com>
2012-09-27 18:21:27 -0700

    calculated all dX with P<mu_ -> P=0 (pre-test)

--- ss_dy.m
+++ ss_dy.m
@@ -1,6 +1,6 @@
 function dy = ss_dy(t,y,Pdim1,Ldim1)
 
-global r_ h_ sigma_ de_ f_ k_ c gammas1D lambdas1D;
+global r_ h_ sigma_ de_ f_ k_ c gammas1D lambdas1D mu_;
 
 % create separate P, N, E, M vectors
 P = y(1:Pdim1);
@@ -21,6 +21,16 @@ mrates = zeros(Pdim1,Pdim1);
         iloss = sum(mrates(1,:));
         mrates(1,1) = 1-iloss;
     
+    
+% enforcing P cutoff for calculating everything...
+%Pis0 = zeros(Pdim1,1);
+for i=1:Pdim1
+    if(P(i)<mu_)
+        P(i)=0;
+%        Pis0(i)=1;
+    end
+end
+        
 % calculate dP (all size Pdim1 x 1)        
 dmut = zeros(Pdim1,1);
 omega = zeros(Pdim1,1);
