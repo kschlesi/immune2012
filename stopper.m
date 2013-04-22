50f4f0e2d3db0e2d5e7144a0a25e9192a4af34d3
Kimberly <kschlesi42@gmail.com>
2013-04-22 13:15:44 -0700

    added different cutoff options to stopper

--- stopper.m
+++ stopper.m
@@ -1,10 +1,17 @@
-function [value,isterminal,direction] = stopper(t,y,Pdim1)
+function [value,isterminal,direction] = stopper(t,y,mu_)
 
-global mu_ ;
-
-value = zeros(Pdim1);
-for i=1:Pdim1
+value = zeros(size(y,1),1);
+for i=1:size(y,1)
     value(i) = y(i)-mu_;
 end
-isterminal = ones(Pdim1);
-direction = (-1)*ones(Pdim1);
+isterminal = ones(size(y,1),1);
+direction = (-1)*ones(size(y,1),1);
+
+% global Pdim1 ;       %% uncomment here to stop ONLY FOR P
+% 
+% value = zeros(Pdim1,1);
+% for i=1:Pdim1
+%     value(i) = y(i)-mu_;
+% end
+% isterminal = ones(Pdim1,1);
+% direction = (-1)*ones(Pdim1,1);
