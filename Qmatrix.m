b6711ad38cbe84ba7e523486d0814f58b2061328
Kimberly <kschlesi42@gmail.com>
2013-06-17 09:38:07 -0700

    added explicit normalization test in Qmatrix

--- Qmatrix.m
+++ Qmatrix.m
@@ -4,6 +4,7 @@ mrates = zeros(Pdim1,Pdim1);
 isnormz = 0;
 
 while ~isnormz
+    isnormz = 1;
     for i=2:Pdim1
         for j=1:i-1
             mrates(i,j) = (1/Pdim1)*abs(randn/(i-j)^c)/chi_; % mutation probability
@@ -11,19 +12,16 @@ while ~isnormz
         end
         iloss = sum(mrates(i,:));
         if iloss >= 1
-            error('probabilities failed!!!');
-        else
-            isnormz = 1;
+            isnormz = 0;
         end
         mrates(i,i) = 1-iloss;
     end
     iloss = sum(mrates(1,:));
     if iloss >= 1
-        error('probabilities failed!!!');
-    else
-        isnormz = 1;
+        isnormz = 0;
     end
     mrates(1,1) = 1-iloss;
+    isnormz
 end
 
 end
