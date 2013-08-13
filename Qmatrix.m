aa4387ac211a533043f42d0cd839c02383dfb3ea
Kimberly <kschlesi42@gmail.com>
2013-08-13 16:28:13 -0700

    fixed typos in Qmatrix

--- Qmatrix.m
+++ Qmatrix.m
@@ -2,13 +2,22 @@ function mrates = Qmatrix(Pdim1,chi_)
 
 notnormz = 1;
 iminj = repmat((1:Pdim1)',1,Pdim1)-repmat(1:Pdim1,Pdim1,1)+eye(Pdim1);
-while notnormz
-    notnormz = 0;
-    mrates = (1/(Pdim1*chi_)).*abs(randn(Pdim1))./(iminj.^2).*(triu(ones(Pdim1),1));
-    mrates = mrates + triu(mrates,1)';
-    iloss = 1-sum(mrates,1);   % if unsymmetrising, check this!!!!
-    notnormz = sum((iloss>1));
-end    
-mrates = mrates + diag(iloss);
+
+if ~chi_
+    mrates = zeros(Pdim1);
+    % uncomment below if combined mutation and growth terms
+    %mrates = eye(Pdim1);
+else
+    while notnormz
+        notnormz = 0;
+        mrates = (1/(Pdim1*chi_)).*abs(randn(Pdim1))./(iminj.^2).*(triu(ones(Pdim1),1));
+        mrates = mrates + triu(mrates,1)';
+        iloss = sum(mrates,1);   % if unsymmetrising, check this!!!!
+        notnormz = sum((iloss>1));
+    end
+    % uncomment below if combined mutation and growth terms
+    %mrates = mrates + diag(1-iloss);
+end
+    
 
 end
