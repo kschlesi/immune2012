619d60289569c8e877455be70345e5b3a1ec9015
Kimberly <kschlesi42@gmail.com>
2013-08-14 22:55:49 -0700

    created nearest-neighbor Qmatrix

--- Qmatrix.m
+++ Qmatrix.m
@@ -1,8 +1,5 @@
 function mrates = Qmatrix(Pdim1,chi_,spliton)
 
-notnormz = 1;
-iminj = repmat((1:Pdim1)',1,Pdim1)-repmat(1:Pdim1,Pdim1,1)+eye(Pdim1);
-
 if ~chi_
     mrates = zeros(Pdim1);
     if ~spliton
@@ -10,18 +7,16 @@ if ~chi_
         mrates = eye(Pdim1);
     end
 else
-    while notnormz
-        notnormz = 0;
-        mrates = (1/(Pdim1*chi_)).*abs(randn(Pdim1))./(iminj.^2).*(triu(ones(Pdim1),1));
-        mrates = mrates + triu(mrates,1)';
-        iloss = sum(mrates,1);   % if unsymmetrising, check this!!!!
-        notnormz = sum((iloss>1));
-    end
+    mrates = diag(chi_*ones(Pdim1-1,1),1) + diag(chi_*ones(Pdim1-1,1),-1);
+    iloss = sum(mrates,2);
+    disp(mrates);
     if ~spliton
         % uncomment below if combined mutation and growth terms
         mrates = mrates + diag(1-iloss);
     end
 end
-    
+if (sum(sum(mrates<0))||(sum(iloss>1)))
+    error('Qmatrix not normalized!')
+end
 
 end
