5c3544d8c07fb05339c8c372c2be5c33321762dc
Kimberly <kschlesi42@gmail.com>
2013-10-25 13:38:07 -0700

    worked on phaseplot plotting; needs improvement

--- PHASEPLOT.m
+++ PHASEPLOT.m
@@ -2,10 +2,16 @@
 % in which plots are made of the phase diagrams that have appropriate
 % colors and things.
 
-twovars = 'bchi';
-xvar = 'b';
-yvar = 'chi_';
-windowsize = 0.25;
+% twovars = 'bchi';
+% xvar = 'b';
+% yvar = 'chi_';
+% windowsize = 0.25; % y-resolution
+% xresolution = 1;
+twovars = 'Gammabeta';
+xvar = 'one';
+yvar = 'two';
+windowsize = 0.25; % y-resolution
+xresolution = 0.025e-4;
 
 % first, we load the necessary files
 orig_tests = csvread(['/Users/kimberly/Google Drive/immunedata/PL13/'...
@@ -16,7 +22,7 @@ orig_tests = csvread(['/Users/kimberly/Google Drive/immunedata/PL13/'...
         yaxis = ymin:windowsize:ymax;
         xmin = min(orig_tests(:,1));
         xmax = max(orig_tests(:,1));
-        xaxis = xmin:xmax;
+        xaxis = xmin:xresolution:xmax;
         %disp([xmin xmax ymin ymax]);
         figure
         escapes = orig_tests(:,2).*(orig_tests(:,3)==1);
@@ -29,8 +35,8 @@ orig_tests = csvread(['/Users/kimberly/Google Drive/immunedata/PL13/'...
         
 orig_result = csvread(['/Users/kimberly/Google Drive/immunedata/PL13/'...
             twovars '_phaseline.txt']);
-orig_result1 = csvread(['/Users/kimberly/Google Drive/immunedata/PL13/'...
-            twovars '_phaseline1.txt']);
+% orig_result1 = csvread(['/Users/kimberly/Google Drive/immunedata/PL13/'...
+%             twovars '_phaseline1.txt']);
 % sort results individually...
 %orig_result = sortentry(orig_result,'col',1);
 %orig_result1 = sortentry(orig_result1,'col',1);
@@ -44,7 +50,22 @@ orig_result = sortentry(orig_result,'col',1);
 % next, we define the separate regions between the phaselines.
 % like so:
 % FOR EACH x-axis number (listed in orig_tests(:,1))
-%       determine how many y-axis crossings exist
+%       determine how many y-axis crossings exist (xcounts)
 % 
 
 % finally, we create the plot.
+% figure out how many xes there are.
+xcounts = histc(orig_result(:,1),xaxis)';
+phasmat = zeros(size(yaxis,2),size(xaxis,2)); % includes all tested points
+for xval=1:size(xaxis,2)
+    num_lines = 0;
+    while num_lines<xcounts(xval)
+        phasmat(:,xval) = phasmat(:,xval) + ...
+            ((yaxis')>=orig_result(xval,3)) - ((yaxis')<=orig_result(xval,2));
+        num_lines = num_lines+1;
+    end
+end
+
+figure
+v = [-2 -1 0 1 2 3];
+contourf(xaxis,(yaxis'),phasmat);
